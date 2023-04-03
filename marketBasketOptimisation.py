05b1e6284a34c69d9932b608bec8057409a6e79c
SunAltahri <sunshine.arts.3@gmail.com>
2023-04-03 10:08:32 +0300

    edit

--- marketBasketOptimisation.py
+++ marketBasketOptimisation.py
@@ -1,18 +1,27 @@
+
+#Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from apyori import apriori
 import seaborn as sns
 
-# قراءة ملف CSV وإعداد بيانات المدخلات لـ Apriori
+#Data Preprocessing
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
 transactions = []
 for i in range(0, len(dataset)):
     transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
 
-# تدريب نموذج Apriori على المجموعة البيانات
+# Training the Apriori model on the dataset
 rules = apriori(transactions=transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2, max_length=2)
 
+#_______Visualising the results___________________
+
+#Displaying the first results coming directly from the output of the apriori function
+#results = list(rules)
+
+
+#Putting the results well organised into a Pandas DataFrame
 # تحويل النتائج إلى DataFrame
 results_df = pd.DataFrame(columns=('Rule', 'Support', 'Confidence', 'Lift'))
 
@@ -25,15 +34,21 @@ for rule in rules:
         lift = order.lift
         results_df.loc[len(results_df)] = [f"{lhs} => {rhs}", support, confidence, lift]
 
-# إضافة عمود للقواعد المهمة
+# Add a column for interesting rules
 results_df['Interesting'] = np.where(results_df['Lift'] >= 4, '√', 'X')
 
 # عرض النتائج
+#Displaying the results non sorted
+#resultsinDataFrame
+#Displaying the results sorted by descending lifts
+#resultsinDataFrame.nlargest(n = 10, columns = 'Lift')
+
+# عرض النتائج
 if len(results_df) > 0:
-    print("النتائج:")
+    print("results:")
     print(results_df.sort_values(by=['Lift'], ascending=False)[['Rule', 'Support', 'Confidence', 'Lift', 'Interesting']])
 else:
-    print("لا يوجد نتائج.")
+    print(" There are no results.")
 
 # إظهار مخطط الإحداثيات
 sns.scatterplot(x='Support', y='Lift', hue='Interesting', data=results_df)
